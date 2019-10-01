97b5b7ab7eac6e923b2228c0da09bcce8a8a4671
nkflores <nkflores@hotmail.com>
2019-10-01 00:00:25 -0600

    updated files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,57 @@
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
-"j", "k", "l", "m", "n", "o", "p", "q", "r", "s", 
-"t", "u", "v", "w", "x", "y", "z"];
-
-var guessedLetters = [];
-
-var letterToGuess = null;
-//var letterToGuess = ["a", "e", "i", "o", "u"];
-
-var guessesLeft = 9;
-
+var options = ["a", "e", "i", "o", "u"];
 var wins = 0;
 var losses = 0;
+var guessesLeft = 9;
+var guessedLetters = [];
 
-var updateGuessesLeft = function() {
+  
+var upgradeWins = function() {
+  document.querySelector("#wins").innerHTML.wins;
+};
+var upgradeLosses = function() {
+  document.querySelector("#losses").innerHTML.losses;
+}
+var updageRemainingGuesses = function() {
   document.querySelector("#guesses-left").innerHTML = guessesLeft;
 };
-
-var updateLetterToGuess = function() {
-  letterToGuess = letters[Math.floor(Math.random() * letters.length)];
-};
-
 var updateGuessesSoFar = function() {
-  document.querySelector("#your-guesses").innerHTML = guessedLetters.join(", ");
+    document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(", ");
 };
-
 var reset = function() {
   guessesLeft = 9;
   guessedLetters = [];
-  updateLetterToGuess();
-  updateGuessesLeft();
-  updateGuessesSoFar();
-};
-
-updateLetterToGuess();
-updateGuessesLeft();
+}
 
 document.onkeydown = function(event) {
+  var userpick = event.key.toLocaleLowerCase();
   guessesLeft--;
 
-  var letter = event.key.toLowerCase();
-
-  guessedLetters.push(letter);
-
-  updateGuessesLeft();
+  var computerpick = options[Math.floor(Math.random()*options.length)]; 
+  console.log(computerpick);
+  
+  guessedLetters.push(userpick);
+  
+  upgradeWins();
+  upgradeWins();
+  updageRemainingGuesses();
   updateGuessesSoFar();
 
-
-  if (letter === letterToGuess) {
-
+  if (userpick === computerpick) {
     wins++;
     document.querySelector("#wins").innerHTML = wins;
-
     reset();
   }
-
+  
   if (guessesLeft === 0) {
-
     losses++;
     document.querySelector("#losses").innerHTML = losses;
-
     reset();
   }
-};
+
+
+}
+
+
+  
+  
+  
